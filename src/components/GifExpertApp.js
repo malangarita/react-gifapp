29bf1c0ceb670b0f9c207c8c198d402de863a7c8
Miguel Angel Barba <miguel.angel.langarita@gmail.com>
2022-01-31 18:45:35 +0100

    Commit Inicial

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(["Matrix"]);
+
+  return (
+    <>
+        <h2>GifExpertApp</h2>
+        <AddCategory setCategories={setCategories}/>
+        <hr/>
+        <ol>
+            {
+                categories.map( category =>{
+                        return <GifGrid
+                                key = {category}
+                                category={category}/>
+                    }
+                )
+            }
+        </ol>
+    </>
+  )};
